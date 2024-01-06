c2571997827dab9dfe8cf69c378dfba739c87279
Wysmith00 <wysmith00@gmail.com>
2024-01-06 16:04:05 -0500

    front end reworked

--- script.js
+++ script.js
@@ -22,38 +22,15 @@ function fetchAllBlogPosts() {
 
 function displayBlogPosts(posts) {
     const postsContainer = document.getElementById('blog-posts');
-    postsContainer.innerHTML = ''; // Clear current posts
+    postsContainer.innerHTML = ''; 
     posts.forEach(post => {
         const postElement = document.createElement('div');
         postElement.innerHTML = `
             <h3>${post.title}</h3>
             <p>${post.content}</p>
-            <button onclick="updateBlogPost('${post._id}')">Update</button>
-            <button onclick="deleteBlogPost('${post._id}')">Delete</button>
         `;
         postsContainer.appendChild(postElement);
     });
-}
-
-function updateBlogPost(postId) {
-    const title = prompt("New Title:");
-    const content = prompt("New Content:");
-
-    axios.put(`/api/bloginfos/${postId}`, { title, content })
-        .then(response => {
-            console.log(response);
-            fetchAllBlogPosts();
-        })
-        .catch(error => console.error(error));
-}
-
-function deleteBlogPost(postId) {
-    axios.delete(`/api/bloginfos/${postId}`)
-        .then(response => {
-            console.log(response);
-            fetchAllBlogPosts();
-        })
-        .catch(error => console.error(error));
 }
 
 function createWorkout() {
@@ -63,7 +40,7 @@ function createWorkout() {
 
     axios.post('/api/workouts', { type, duration, notes })
         .then(response => {
-            console.log(respnose);
+            console.log(response);
             fetchAllWorkouts();
         })
         .catch(error => console.error(error));
@@ -86,40 +63,12 @@ function displayWorkouts(workouts) {
             <h3>${workout.type}</h3>
             <p>Duration: ${workout.duration} minutes</p>
             <p>Notes: ${workout.notes}</p>
-            <button onclick="updateWorkout('${workout._id}')">Update</button>
-            <button onclick="deleteWorkout('${workout._id}')">Delete</button>
         `;
         workoutsContainer.appendChild(workoutElement);
     });
 }    
 
-function updateWorkout(workoutId) {
-    const type = prompt("New Workout Type:");
-    const duration = prompt("New Duration (in minutes):");
-    const notes = prompt ("New Notes:");
-
-    axios.put(`/api/workouts/${workoutId}`, { type, duration, notes })
-        .then(response => {
-            console.log(response);
-            fetchAllWorkouts();
-        })
-        .catch(error => console.error(error));
-}
-
-function deleteWorkout(workoutId) {
-    axios.delete(`/api/workouts/${workoutId}`)
-        .then(response => {
-            console.log(response);
-            fetchAllWorkouts();
-        })
-        .catch(error => console.error(error));
-}
-
 document.addEventListener('DOMContentLoaded', function() {
     fetchAllBlogPosts();
     fetchAllWorkouts();
 });
-
-
-
-
